825f2792ef0a895c564a5d10e5310e2432bf92e3
Matti Jagula <matti.jagula@gmail.com>
2015-12-19 14:32:57 +0200

    Extracted the game loop out of connectivity

---
+++ src/mazerace/game.clj
@@ -0,0 +1,73 @@
+(ns mazerace.game
+  (:require [clojure.tools.logging :as log]
+            [clojure.core.async :as a :refer [>! <! >!! <!! go chan close! thread alts! alts!! timeout go-loop]]
+            [mazerace.maze :as maze]
+            [mazerace.maze :as maze]))
+
+(defn the-other [player]
+  (if (= :p1 player) :p2 :p1))
+
+(defn- place-randomly [game player]                         ; avoid the other player and the exit
+  (let [other-pos (get-in game [(the-other player) :position])
+        width (count (get-in game [:maze 0]))
+        heigth (count (get-in game [:maze]))]
+    (loop []
+      (let [rand-x (rand-int width)
+            rand-y (rand-int heigth)]
+        (if (not= [rand-x rand-y] other-pos)
+          [rand-x rand-y]
+          (recur))))))
+
+(defn handle-game [[recv-a send-a] [recv-b send-b]]
+  (log/info "Got pair, launching handler")
+  (go
+    (let [width 20
+          height 20
+          target-position [(quot width 2) (dec height)]
+          maze (maze/generate width height)
+          game (atom {:maze   maze
+                      :p1     {:position [0 0]}
+                      :p2     {:position [(dec width) 0]}
+                      :target target-position})]
+      (>! send-a {:maze              maze
+                  :position          (get-in @game [:p1 :position])
+                  :opponent-position (get-in @game [:p2 :position])
+                  :target            target-position})
+      (>! send-b {:maze              maze
+                  :position          (get-in @game [:p2 :position])
+                  :opponent-position (get-in @game [:p1 :position])
+                  :target            target-position})
+      (loop []
+        (let [[msg chan] (alts! [recv-a recv-b])
+              player (if (= chan recv-a) :p1 :p2)
+              other-player (the-other player)]
+          (if msg
+            (do
+              (log/info "Received " msg "from" player)
+              (when (:move msg)
+                ; TODO: add sanity check that the new position is 1 away from previous
+                (let [player-pos (:move msg)
+                      other-pos (get-in @game [other-player :position])]
+                  (log/debug player player-pos other-player other-pos)
+                  (if (= player-pos other-pos)
+                    (do
+                      (log/info "Collision, randomizing positions")
+                      (swap! game assoc-in [player :position] (place-randomly @game player))
+                      (swap! game assoc-in [other-player :position] (place-randomly @game other-player))
+                      (>! (if (= chan recv-a) send-a send-b) {:position          (get-in @game [player :position])
+                                                              :opponent-position (get-in @game [other-player :position])})
+                      (>! (if (= chan recv-a) send-b send-a) {:position          (get-in @game [other-player :position])
+                                                              :opponent-position (get-in @game [player :position])}))
+                    (if (= player-pos target-position)
+                      (do
+                        (log/info player "wins")
+                        (>! (if (= player :p1) send-a send-b) {:result :win})
+                        (>! (if (= player :p1) send-b send-a) {:result :lose}))
+                      (do
+                        (swap! game assoc-in [player :position] (:move msg))
+                        (>! (if (= chan recv-a) send-b send-a) {:opponent-position (:move msg)}))))))
+              ; (log/debug "after" player (get-in @game [player :position]) other-player (get-in @game [other-player :position]))
+              (recur))
+            ((log/info "cleaning up pair")
+              (close! send-a)
+              (close! send-b))))))))
