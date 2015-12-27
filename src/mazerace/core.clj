bb52aa004ae2e7a8f8926d142d56b0f05566f6f5
Matti Jagula <matti.jagula@gmail.com>
2015-12-27 15:40:03 +0200

    Removed finished todo item

--- src/mazerace/core.clj
+++ src/mazerace/core.clj
@@ -11,16 +11,13 @@
             [clojure.tools.logging :as log])
   (:gen-class))
 
-; TODO: move into socket.clj,
-
-
 (defroutes app
            (GET "/" [] (resp/redirect "index.html"))
            (GET "/ws" [] ws/socket-handler)
            (resources "/" {:root "/public"})
            (not-found "Page not found"))
 
-(defn -main [& args]
+(defn -main [& _]
   (let [handler (-> #'app site reload/wrap-reload ring.middleware.keyword-params/wrap-keyword-params ring.middleware.params/wrap-params)
         port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
     (log/info "Starting server on port " port)
