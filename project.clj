fb50d660b0ef999522516c59c9fe973eaf510c03
Matti Jagula <matti.jagula@gmail.com>
2015-12-29 23:08:11 +0200

    Added react-animation dependency

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
                  [ring/ring-devel "1.1.8"]
                  [compojure "1.0.2"]
                  [http-kit "2.1.19"]
-                 [reagent "0.5.1"]
+                 [reagent "0.5.1" :exclusions [cljsjs/react]]
+                 [cljsjs/react-with-addons "0.13.3-0"]
                  [org.clojure/tools.logging "0.2.6"]
                  [log4j/log4j "1.2.17"]]
   :min-lein-version "2.0.0"
