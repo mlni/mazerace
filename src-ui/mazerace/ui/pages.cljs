f820479a0e744fad0387f149c66540c3cc4d44fb
Matti Jagula <matti.jagula@gmail.com>
2016-01-01 23:56:30 +0200

    Fixed size of maze on render

--- src-ui/mazerace/ui/pages.cljs
+++ src-ui/mazerace/ui/pages.cljs
@@ -1,7 +1,10 @@
 (ns mazerace.ui.pages
-  (:require [mazerace.game :as game]
+  (:require [reagent.core :as r]
+            [mazerace.game :as game]
             [mazerace.ui.svg :as svg]
-            [mazerace.socket :as ws]))
+            [mazerace.socket :as ws]
+            [mazerace.window :as window]
+            [mazerace.log :as log]))
 
 (defn index-page []
   [:div
@@ -9,7 +12,7 @@
    [:button {:on-click #(game/start-game!)}
     "Play!"]])
 
-(defn- overlay [& content]
+(defn- overlay [content]
   [:div
    [:div.overlay-background]
    [:div.overlay-message
@@ -17,33 +20,50 @@
 
 (defn result [result]
   (overlay
-    [:div (condp = result
-            "win" "You win!"
-            "lose" "Opponent won!"
-            "opponent-disconnected" "Opponent disconnected"
-            "Game over!")]
-    [:div [:button {:on-click #(game/start-game!)} "Play again!"]]))
+    [:div
+     (condp = result
+       "win" "You win!"
+       "lose" "Opponent won!"
+       "opponent-disconnected" "Opponent disconnected"
+       "Game over!")
+     [:div
+      [:button {:on-click #(game/start-game!)} "Play again!"]]]))
 
 (defn connection-lost []
-  (overlay "Opponent disconnected"))
+  (overlay [:div "Opponent disconnected"]))
 
-(defn play [game]
-  (let [connection-state (ws/state)]
-    [:div.game
-     (if (:result game)
-       [result (:result game)]
-       (if (not (:connected connection-state))
-         [connection-lost]))
-     (when (:maze game)
-       [svg/render-maze game])]))
+(defn play []
+  (let [game (game/game-state)
+        connection-state (ws/state)]
+    [:div.game-wrapper
+     [:div.game-container
+      (if (:result game)
+        [result (:result game)]
+        (if (not (:connected connection-state))
+          [connection-lost]))
+      (when (:maze game)
+        [svg/render-maze game])]]))
+
+(defn play-component [game]
+  (r/create-class
+    {:component-did-mount window/on-resize-window
+     :reagent-render      play}))
 
 (defn connecting []
   ; TODO: handle "connecting" state
   [:h1 "Waiting for an opponent"])
 
+(defn navbar []
+  [:nav {:class "navbar navbar-default navbar-fixed-top"}
+   [:div.container
+    [:div.navbar-header
+     [:a.navbar-brand "Mazerace"]]]])
+
 (defn content []
   (let [game (game/game-state)]
-    [:div.container
-     (condp = (:state game) :connecting [connecting]
-                            :playing [play game]
-                            [index-page])]))
+    [:div
+     [navbar]
+     [:div.container
+      (condp = (:state game) :connecting [connecting]
+                             :playing [play-component game]
+                             [index-page])]]))
--- src-ui/mazerace/window.cljs
+++ src-ui/mazerace/window.cljs
@@ -1,19 +1,19 @@
 (ns mazerace.window
   (:require [mazerace.log :as log]))
 
-(defn resize-window []
+(defn on-resize-window []
   (let [h (aget js/window "innerHeight")
         w (aget js/window "innerWidth")
-        container (aget (.getElementsByClassName js/document "container") 0)]
+        size (max 100
+                  (- (min w h) 50))
+        container (aget (.getElementsByClassName js/document "game-container") 0)]
     (when container
-      (log/info (str "on-resize " h " " w " " container (str (min w h) "px")))
-      (set! (.-width (.-style container)) (str (min w h) "px")))))
+      (log/info (str "on-resize " h " " w " " container size))
+      (set! (.-width (.-style container)) (str size "px")))))
 
 (defn handle-resize! []
   (let [timeout (atom nil)]
     (.addEventListener js/window "resize"
                        (fn [_]
                          (when @timeout (js/clearTimeout @timeout))
-                         (reset! timeout (js/setTimeout resize-window 100))))
-    (js/setTimeout resize-window 100)                       ; run once on startup as well
-    ))
+                         (reset! timeout (js/setTimeout on-resize-window 100))))))
