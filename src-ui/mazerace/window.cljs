15153e4078fbc91d186bf7c031a769da4a5e5304
Matti Jagula <matti.jagula@gmail.com>
2016-01-02 20:35:06 +0200

    Don't resize the maze outside of the range bootstrap expects

--- src-ui/mazerace/window.cljs
+++ src-ui/mazerace/window.cljs
@@ -1,15 +1,13 @@
-(ns mazerace.window
-  (:require [mazerace.log :as log]))
+(ns mazerace.window)
 
 (defn on-resize-window []
-  (let [h (aget js/window "innerHeight")
-        w (aget js/window "innerWidth")
-        size (max 100
-                  (- (min w h) 50))
-        container (aget (.getElementsByClassName js/document "game-container") 0)]
-    (when container
-      (log/info (str "on-resize " h " " w " " container size))
-      (set! (.-width (.-style container)) (str size "px")))))
+  (when-let [wrapper (aget (.getElementsByClassName js/document "game-wrapper") 0)]
+    (let [h (aget js/window "innerHeight")
+          w (aget wrapper "offsetWidth")
+          size (max 100 (min w h))
+          container (aget (.getElementsByClassName js/document "game-container") 0)]
+      (when container
+        (set! (.-width (.-style container)) (str size "px"))))))
 
 (defn handle-resize! []
   (let [timeout (atom nil)]
