8ac5da6503a8ad3af203546cc3b78c73ff995999
Matti Jagula <matti.jagula@gmail.com>
2016-01-04 23:41:14 +0200

    Added github link

--- src-ui/mazerace/ui/pages.cljs
+++ src-ui/mazerace/ui/pages.cljs
@@ -87,6 +87,10 @@
   (let [game (game/game-state)]
     [:div
      [navbar]
+     [:a.github {:href "https://github.com/mlni/mazerace"}
+      [:img {:src (str "https://camo.githubusercontent.com/e7bbb0521b397edbd5fe43e7f760759336b5e05f/68747470733a2f2f73"
+                       "332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265"
+                       "656e5f3030373230302e706e67")}]]
      [:div.container
       (condp = (:state game) :connecting [connecting]
                              :playing [play-component]
